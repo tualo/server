7f57fd75f88259de60800ca9aac82ef192d1f8ca
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-04-18 09:04:03 +0200

    config load by ini file

--- src/Server.php
+++ src/Server.php
@@ -13,15 +13,9 @@ class Server{
         TualoApplication::set('basePath', dirname($_SERVER['SCRIPT_FILENAME']) );
         TualoApplication::set('cachePath', TualoApplication::get('basePath').'/cache/' );
         TualoApplication::set('configurationFile',TualoApplication::get('basePath').'/configuration/.htconfig');
-
-        $settings = parse_ini_file(TualoApplication::get('configurationFile'));
-        foreach($settings as $key=>$value){ if(!defined($key)){ define($key,$value); } TualoApplication::set($key,$value);}
-        if (!defined('BASIC_COMPONENTS')) define('BASIC_COMPONENTS','cmp_bsc cmp_ext_6 cmp_ext cmp_tualo cmp_tualo_theme cmp_ds cmp_dashboard cmp_ext_6_ext-theme-tualo');
-
-        // if (defined('__USE_DB_SESSION_HANLDER__')&&(__USE_DB_SESSION_HANLDER__==1)) session_set_save_handler(new TualoDBSessionHandler(), true);
-
-        
+        $settings = parse_ini_file((string)TualoApplication::get('configurationFile'),true);
+        TualoApplication::set('configuration',$settings);
+        //foreach($settings as $key=>$value){ if(!defined($key)){ define($key,$value); } TualoApplication::set($key,$value);}
         TualoApplication::run(); // run all middlewares
-    
     }
 }
