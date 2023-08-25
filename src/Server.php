11791494afcf836f3f71a68524eb4b48b365cdad
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-08-25 15:57:12 +0200

    fixes

--- src/Server.php
+++ src/Server.php
@@ -7,14 +7,16 @@ use Tualo\Office\Basic\Route;
 use Tualo\Office\Basic\TualoDBSessionHandler;
 use tualo\Office\Basic\Middleware\Maintaince;
 
-class Server{
-    public function run(){
-        TualoApplication::set('requestPath',dirname($_SERVER["SCRIPT_NAME"]));
-        TualoApplication::set('basePath', dirname($_SERVER['SCRIPT_FILENAME']) );
-        TualoApplication::set('cachePath', TualoApplication::get('basePath').'/cache/' );
-        TualoApplication::set('configurationFile',TualoApplication::get('basePath').'/configuration/.htconfig');
-        $settings = parse_ini_file((string)TualoApplication::get('configurationFile'),true);
-        TualoApplication::set('configuration',$settings);
+class Server
+{
+    public function run()
+    {
+        TualoApplication::set('requestPath', dirname($_SERVER["SCRIPT_NAME"]));
+        TualoApplication::set('basePath', dirname($_SERVER['SCRIPT_FILENAME']));
+        TualoApplication::set('cachePath', TualoApplication::get('basePath') . '/cache/');
+        TualoApplication::set('configurationFile', TualoApplication::get('basePath') . '/configuration/.htconfig');
+        $settings = parse_ini_file((string)TualoApplication::get('configurationFile'), true);
+        TualoApplication::set('configuration', $settings);
         //foreach($settings as $key=>$value){ if(!defined($key)){ define($key,$value); } TualoApplication::set($key,$value);}
         TualoApplication::run(); // run all middlewares
     }
