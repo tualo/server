6048e19d555fb47f6e7f1c68d0616a88604b0384
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-04-18 08:00:45 +0200

    fix

--- src/Server.php
+++ src/Server.php
@@ -67,7 +67,7 @@ class Server
     {
 
         //TualoApplication::set('requestPath', dirname($_SERVER["REQUEST_URI"]));
-        TualoApplication::set('requestPath', dirname($_SERVER["SCRIPT_NAME"]) . '/');
+        TualoApplication::set('requestPath', dirname($_SERVER["SCRIPT_NAME"]));
         TualoApplication::set('basePath', dirname($_SERVER['SCRIPT_FILENAME']));
         TualoApplication::set('cachePath', TualoApplication::get('basePath') . '/cache/');
         TualoApplication::set('configurationFile', TualoApplication::get('basePath') . '/configuration/.htconfig');
